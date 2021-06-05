6177eb50c33f6d56a075eb515c8be8963ee578c4
Ivaylo Filipov <ivaylofilipov.72@gmail.com>
2021-06-05 12:34:30 +0300

    Add new config class DesignTimeDbContextFactory

--- BMS.Data/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace BMS.Data
-{
-    public class Class1
-    {
-    }
-}
---
+++ BMS.Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace BMS.Data
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
